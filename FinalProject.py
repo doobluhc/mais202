1b4aa286f831be24da0f1ddbc86058e006c3aef9
doobluhc <49000558+doobluhc@users.noreply.github.com>
2020-03-26 14:34:41 -0400

    need to make it more accurate

--- FinalProject.py
+++ FinalProject.py
@@ -1,56 +1,79 @@
-import pandas as pd
-import io
-import numpy as np
 import matplotlib
-matplotlib.use('TkAgg')
-from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
+matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn import metrics, svm
+import cPickle
+from sklearn.datasets.samples_generator import make_blobs
+import pylab as pl
+from sklearn.utils import shuffle
+#preporcess data
+features_to_drop = ['Unnamed: 0', 'Game', 'Date', 'Opponent', 'OpponentPoints',
+                        'Opp.FieldGoals', 'Opp.3PointShotsAttempted',
+                        'Opp.FreeThrowsAttempted', 'Opp.FreeThrows.', 'Opp.OffRebounds',
+                        'Opp.Assists', 'Opp.TotalFouls',
+                        'Opp.FieldGoalsAttempted', 'Opp.3PointShots','FreeThrows','FieldGoals','X3PointShots'
+                    ,'X3PointShotsAttempted','OpponentPoints','TotalFouls']
+NBAdata = pd.read_csv('/Users/chengchen/Downloads/nba.games.stats.csv')
+NBAdata = NBAdata.drop(features_to_drop, axis=1)
+del NBAdata['Team']
+NBAdata['Home'].replace({'Home': 1, 'Away': 0}, inplace=True)
+NBAdata = shuffle(NBAdata)
+results = NBAdata['WINorLOSS']
+
+binaryResults = []
+for i in range(len(results)):
+    if(results[i] == 'W'):
+        binaryResults.append(1)
+    else:
+        binaryResults.append(0)
+binaryResults = np.array(binaryResults)
+del NBAdata['WINorLOSS']
+
+
+
 def logisticRegression():
-    data = pd.read_csv('/Users/chengchen/Downloads/nba.games.stats.csv')
-    #shuffle the data
-    data = data.reindex(np.random.permutation(data.index))
-    #drop useless data
-    features_to_drop = ['Unnamed: 0', 'Game', 'Date', 'Opponent', 'OpponentPoints',
-                        'Opp.FieldGoals', 'Opp.3PointShotsAttempted', 'Opp.3PointShots.', 'Opp.FreeThrows',
-                        'Opp.FreeThrowsAttempted', 'Opp.FreeThrows.', 'Opp.OffRebounds', 'Opp.TotalRebounds',
-                        'Opp.Assists', 'Opp.Steals', 'Opp.Blocks', 'Opp.Turnovers', 'Opp.TotalFouls',
-                        'Opp.FieldGoalsAttempted', 'Opp.FieldGoals.', 'Opp.3PointShots']
-    data = data.drop(features_to_drop, axis=1)
-
-    #Assists
-    X_train_assist, X_test_assist,y_train_assist,y_test_assist = train_test_split(data[['Assists']],data.WINorLOSS,test_size=0.2)
-    model = LogisticRegression()
-    model.fit(X_train_assist,y_train_assist)
-    assistCount = []
-    for outcome in model.predict(X_test_assist):
-        if (outcome == 'W'):
-            assistCount.append(1)
-        else:
-            assistCount.append(0)
+    X_train, X_test, y_train, y_test = train_test_split(NBAdata, binaryResults, test_size=0.2)
+    logreg = LogisticRegression(class_weight='balanced')
+    logreg.fit(X_train, y_train)
+    y_pred = logreg.predict(X_test)
+    print(y_pred)
+    print(y_test)
+    print("Accuracy:", metrics.accuracy_score(y_test, y_pred))
 
 
-    #team points
-    X_train_TeamPoints, X_test_TeamPoints, y_train_TeamPoints, y_test_TeamPoints = train_test_split(data[['TeamPoints']], data.WINorLOSS,
-                                                                                    test_size=0.2)
-    model = LogisticRegression()
-    model.fit(X_train_TeamPoints, y_train_TeamPoints)
-    teamPointsCount = []
-    for outcome in model.predict(X_test_TeamPoints):
-        if (outcome == 'W'):
-            teamPointsCount.append(1)
-        else:
-            teamPointsCount.append(0)
+def SVM ():
+
+    X_train, X_test, y_train, y_test = train_test_split(NBAdata, binaryResults, test_size=0.2)
+
+    #train the model
+    print('trainning')
+    #clf = svm.SVC(kernel='linear')  # Linear Kernel
+    #clf.fit(X_train, y_train)
+
+    #save the model
+    #print('save the model')
+   # with open('/Users/chengchen/PycharmProjects/mais202FinalProject/clf.pkl', 'wb') as fid:
+        #cPickle.dump(clf, fid)
+
+    #load the model
+    with open('/Users/chengchen/PycharmProjects/mais202FinalProject/clf.pkl', 'rb') as fid:
+        clf = cPickle.load(fid)
+
+    y_pred = clf.predict(X_test)
+    print("Accuracy:", metrics.accuracy_score(y_test, y_pred))
+    print(y_pred)
 
 
-
-
-
-    averagePercentage = (model.score(X_test_assist,y_test_assist)
-                         + model.score(X_test_TeamPoints, y_test_TeamPoints))/2
-
-    print(averagePercentage)
-
-logisticRegression()
+    #visulize the data
+    X_train, y_train = make_blobs(n_samples=50, centers=2,
+                                  random_state=0, cluster_std=0.60)
+    plt.scatter(X_train[:, 0], X_train[:, 1], c=y_train, s=50, cmap='autumn');
+    plt.show()
 
 
 
+SVM()
